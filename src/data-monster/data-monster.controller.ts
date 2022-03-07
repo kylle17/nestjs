0c426b759ea3f360d9045f30acc8ccd418221292
DESKTOP-UDVFQRK\ione <kylle@naver.com>
2022-03-07 09:53:57 +0900

    view 탬플릿 추가

--- src/data-monster/data-monster.controller.ts
+++ src/data-monster/data-monster.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Post, Put , Patch } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Patch,
+  Render,
+} from '@nestjs/common';
 import { Body } from '@nestjs/common';
 import { DataMonsterRequestDto } from 'src/domain/data-monster.request.dto';
 import { DataMonsterEntity } from './data-monster.entity';
@@ -6,34 +15,38 @@ import { DataMonsterService } from './data-monster.service';
 
 @Controller('data-monster')
 export class DataMonsterController {
-    constructor(private readonly dataMonsterService:DataMonsterService){}    
+  constructor(private readonly dataMonsterService: DataMonsterService) {}
 
-    @Get()
-    getAll(){
-        return this.dataMonsterService.findAll();
-    }
+  @Get()
+  @Render('index')
+  getAll() {
+    return { message: 'Hello world!' };
+    // return this.dataMonsterService.findAll();
+  }
 
-    @Get(':id')
-    getOne(@Param('id') id:number){
-        return this.dataMonsterService.findOne(id);
-    }
+  @Get(':id')
+  getOne(@Param('id') id: number) {
+    return this.dataMonsterService.findOne(id);
+  }
 
-    @Post()
-    async createMonster( @Body() dataMonsterEntity : DataMonsterEntity ){
-        await this.dataMonsterService.create(dataMonsterEntity);
-    }
+  @Post()
+  async createMonster(@Param() dataMonsterEntity: DataMonsterEntity) {
+    console.log(dataMonsterEntity);
+    await this.dataMonsterService.create(dataMonsterEntity);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: number, @Body() dataMonsterEntity: DataMonsterEntity) {
-        console.log(id);
-        console.log(dataMonsterEntity);
-        return this.dataMonsterService.update( +id , dataMonsterEntity);
-    }
+  @Patch(':id')
+  update(
+    @Param('id') id: number,
+    @Body() dataMonsterEntity: DataMonsterEntity,
+  ) {
+    console.log(id);
+    console.log(dataMonsterEntity);
+    return this.dataMonsterService.update(+id, dataMonsterEntity);
+  }
 
-    @Delete(':id')
-    remove( @Param('id') id:number ){
-        this.dataMonsterService.remove(id);
-    }
-
-
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    this.dataMonsterService.remove(id);
+  }
 }
